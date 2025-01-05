bb66d0b311140f643e1b52500360a78966a1e43e
Sergio <sergiorobayorr@gmail.com>
2025-01-05 15:31:24 -0500

    worker module

---
+++ modules/worker_lambda/main.tf
@@ -0,0 +1,81 @@
+resource "aws_lambda_function" "this" {
+  function_name    = var.function_name
+  handler          = var.handler
+  runtime          = var.runtime
+  filename         = var.filename
+  source_code_hash = var.source_code_hash
+  environment {
+    variables = var.environment_variables
+  }
+  timeout                        = var.timeout
+  memory_size                    = var.memory_size
+  reserved_concurrent_executions = var.reserved_concurrent_executions
+  publish                        = var.publish
+  role                           = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name               = "${var.function_name}-exec-role"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_cloudwatch_log_group" "this" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = 14
+}
+
+resource "aws_iam_policy" "lambda_s3_access" {
+  name        = "${var.function_name}-s3-access-policy"
+  description = "IAM policy for lambda function to access S3"
+  policy      = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:HeadObject",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "*",
+                "arn:aws:s3:::${var.environment_variables["OUTPUT_BUCKET"]}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_access_attachment" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_s3_access.arn
+}
+
+resource "aws_lambda_permission" "allow_sqs" {
+  statement_id  = "AllowExecutionFromSQS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "sqs.amazonaws.com"
+  source_arn    = var.environment_variables["SQS_QUEUE_ARN"]
+}
--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,7 @@ GOOS=linux GOARCH=amd64 go build -o bin/chunker src/chunker/main.go
 echo "Creating chunker.zip..."
 zip -j bin/chunker.zip bin/chunker
 
+echo "Building worker lambda..."
+GOOS=linux GOARCH=amd64 go build -o bin/worker src/worker/main.go
+
 echo "Build complete."
